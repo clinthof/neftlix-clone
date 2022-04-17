92b33e6ff2218c52d54a07c8bd516cfb63ee8557
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-17 15:15:58 -0400

    Begin jumbotron component

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro';
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: ${({ direction }) => direction};
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1100px;
+  margin: auto;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`;
+
+const Container = styled.div``;
+
+export {
+  Inner,
+  Container,
+}
