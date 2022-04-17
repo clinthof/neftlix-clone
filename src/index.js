b583c4c7403c66911ff5e11297554f24de49399f
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-17 17:26:56 -0400

    Wrap app in fragment to add global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import App from './App';
+import { GlobalStyles } from './globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>  
+    <GlobalStyles />  
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
