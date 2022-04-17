61676cf8893a75d17f246027ee45786e08ef507e
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-17 15:16:51 -0400

    Add some basic jumbotron components (subject to change)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Jumbotron from './components/jumbotron';
+import jumboData from './fixtures/jumbo.json';
+
 const App = () => {
   return (
     <div>
-      Hello
+      <Jumbotron.Container>
+        {jumboData.map(item =>
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+            <Jumbotron>{item.alt}</Jumbotron>
+          </Jumbotron>)}
+      </Jumbotron.Container>
     </div>
   );
 }
