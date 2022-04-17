5303cb32f3bcdfcb66d3d0a456900ab717267e2d
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-17 17:25:23 -0400

    Extract JumbotronContainer to its own container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import Jumbotron from './components/jumbotron';
-import jumboData from './fixtures/jumbo.json';
+import { JumbotronContainer } from './containers/Jumbotron';
 
 const App = () => {
   return (
-    <div>
-      <Jumbotron.Container>
-        {jumboData.map(item =>
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-            <Jumbotron>{item.alt}</Jumbotron>
-          </Jumbotron>)}
-      </Jumbotron.Container>
-    </div>
+    <>
+      <JumbotronContainer />
+    </>
   );
 }
 
