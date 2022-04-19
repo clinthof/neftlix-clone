fdeb089dc4ab9ba4e8a86f63cb246b74efcea0e3
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-19 00:24:21 -0400

    Add FAQs to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { JumbotronContainer } from './containers/Jumbotron';
 import { FooterContainer } from './containers/Footer';
+import { FAQContainer } from './containers/FAQs';
 
 const App = () => {
   return (
     <>
       <JumbotronContainer />
+      <FAQContainer />
       <FooterContainer />
     </>
   );
