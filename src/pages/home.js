31d6263d454f4645416fdee636cd08404d6ce5e5
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-24 15:52:55 -0400

    Wrap home containers in header container

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,16 @@
 import { JumbotronContainer } from '../containers/Jumbotron';
 import { FooterContainer } from '../containers/Footer';
 import { FAQContainer } from '../containers/FAQs';
+import { HeaderContainer } from '../containers/Header';
 
 const Home = () => { 
   return (
     <>
-      <JumbotronContainer />
-      <FAQContainer />
-      <FooterContainer />
+      <HeaderContainer>
+        <JumbotronContainer />
+        <FAQContainer />
+        <FooterContainer />
+      </HeaderContainer>
     </>
   );
 };
