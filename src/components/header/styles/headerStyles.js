b75a902a3f6a4cddce0770bc1fb726f04c72f91f
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-24 19:16:57 -0400

    Finish up header styling (for now)

--- src/components/header/styles/headerStyles.js
+++ src/components/header/styles/headerStyles.js
@@ -2,7 +2,9 @@ import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 const Background = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
 `;
 
 const Frame = styled.div`
@@ -10,11 +12,31 @@ const Frame = styled.div`
 `;
 
 const Container = styled.div`
+  display: flex;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
+  justify-content: space-between;
+  align-items: center;
 
+  a {
+    display: flex;
+  }
+
+  @media (max-width: 1000px) {
+    margin: 0 30px;
+  }
 `;
 
 const Logo = styled.img`
+  height: 2.25rem;
+  width: 8.375rem;
+  margin-right: 40px;
 
+  @media (max-width: 1000px) {
+    height: 45px;
+    width: 167px;
+  }
 `;
 
 const ButtonLink = styled(ReactRouterLink)`
