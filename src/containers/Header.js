37ff1c7be215c810035241aefcadf20d20884160
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-24 15:52:10 -0400

    Add basic header elements to container

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,16 @@
 import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
 
-const HeaderContainer = () => { 
-
+const HeaderContainer = ({ children }) => { 
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} alt='Netflix' />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+      </Header.Frame>
+      {children}
+    </Header>
+  );
 };
 
 export { HeaderContainer };
