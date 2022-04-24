e1f6f913352809290af8d4e11466953d354ce0c5
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-24 15:50:50 -0400

    Create styles

--- src/components/header/styles/headerStyles.js
+++ src/components/header/styles/headerStyles.js
@@ -1,19 +1,45 @@
 import styled from 'styled-components/macro';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+const Background = styled.div`
+
+`;
 
 const Frame = styled.div`
 
 `;
 
-const Logo = styled.div`
+const Container = styled.div`
 
 `;
 
-const Button = styled.button`
+const Logo = styled.img`
 
+`;
+
+const ButtonLink = styled(ReactRouterLink)`
+  display: block;
+  background-color: #e60914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
+
+  &:hover {
+    background-color: #f40612;
+  }
 `;
 
 export {
+  Background,
   Frame,
+  Container,
   Logo,
-  Button,
+  ButtonLink,
 };
