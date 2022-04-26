87ad3db05381fa20b6e02ceeea335615ed459e1d
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-26 14:26:00 -0400

    Set up and seed database

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './globalStyles';
+import { firebase } from './lib/firebase.prod';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
