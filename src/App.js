3c4c53420bd2ab0104136ce9be937496a9e40df1
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-26 22:40:48 -0400

    Add switch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </Route>
+      </Switch>
     </Router>
   );
 }
