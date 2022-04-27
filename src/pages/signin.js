9ea9c4aa361805e6f3c6aa25e67fb71a1e209054
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-26 22:43:39 -0400

    Begin signin component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+import { HeaderContainer, FooterContainer } from '../containers';
+import { Form } from '../components';
+
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignIn = (e) => {
+
+  };
+
   return (
-    <>Hello from signin</>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={ e => handleSignIn(e)}>
+
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 };
 
