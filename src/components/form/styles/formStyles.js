c4f3e61e3396746e5ec7bca9a0c4fe0f30022665
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-28 12:57:40 -0400

    Finish up signin page

--- src/components/form/styles/formStyles.js
+++ src/components/form/styles/formStyles.js
@@ -2,23 +2,45 @@ import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  min-height: 660px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto;
+  max-width: 450px;
+  padding: 60px 68px 40px;
+  margin-bottom: 100px;
 `;
 
 const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  width:100%;
 
 `;
 
 const Title = styled.h1`
-
+  color: #fff;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 28px;
 `;
 
 const Text = styled.p`
-
+  color: #737373;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
 const Subtext = styled.p`
-
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
 `;
 
 const Link = styled(ReactRouterLink)`
@@ -30,16 +52,45 @@ const Link = styled(ReactRouterLink)`
   }
 `;
 
-const Input = styled.div`
+const Input = styled.input`
+  background: #333;
+  border-radius: 4px;
+  border: 0;
+  color: white;
+  height: 50px;
+  line-height: 50px;
+  padding: 5px 20px;
+  margin-bottom: 20px;
 
+  &:focus {
+    outline: none;
+  }
 `;
 
-const Submit = styled.div`
+const Submit = styled.button`
+  background: #e50914;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 24px 0 12px;
+  padding: 16px;
+  color: white;
+  cursor: pointer;
 
+  &:disabled {
+    cursor: auto;
+    opacity: 0.9;
+  }
 `;
 
 const Error = styled.div`
-
+  background: #f40612;
+  border-radius: 4px;
+  font-size: 14px;
+  margin: 0 0 16px;
+  color: white;
+  padding: 15px 20px;
 `;
 
 export {
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,14 +1,34 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { HeaderContainer, FooterContainer } from '../containers';
 import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 const SignIn = () => {
+  const history = useHistory();
+
+  const { firebase } = useContext(FirebaseContext);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
+  
+  const isInvalid = email === '' || password === '';
   const handleSignIn = (e) => {
+    e.preventDefault();
 
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch(err => {
+        setEmail('');
+        setPassword('');
+        setError(err.message);
+      });
   };
 
   return (
@@ -17,9 +37,28 @@ const SignIn = () => {
         <Form>
           <Form.Title>Sign In</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
-          <Form.Base onSubmit={ e => handleSignIn(e)}>
-
+          <Form.Base onSubmit={e => handleSignIn(e)} method='POST'>
+            <Form.Input
+              placeholder='Email address'
+              value={email} 
+              onChange={({target}) => setEmail(target.value)}
+            />
+            <Form.Input
+              placeholder='Password'
+              type='password'
+              autoComplete='off'
+              value={password} 
+              onChange={({target}) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit'>Sign In</Form.Submit>
           </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to='/signup'>Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.Subtext>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot.
+            <Form.Link to='/'> Learn more.</Form.Link>
+          </Form.Subtext>
         </Form>
       </HeaderContainer>
       <FooterContainer />
