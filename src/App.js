8820a661b7629f941dc9179802a607b9a9315caf
clinthof <73394309+clinthof@users.noreply.github.com>
2022-04-30 13:38:29 -0400

    Add auth listener

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Switch, } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute, } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 const App = () => {
-  const user = null;
+  const { user } = useAuthListener();
 
   return (
     <Router>
